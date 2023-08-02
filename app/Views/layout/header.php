65dbd1fcc5c8fbc89d8a2335a2ec5dd4ff707a0d
bisbiss <01newbi@gmail.com>
2023-08-02 10:27:54 +0700

    Update form

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -6,5 +6,6 @@
     <meta name="author" content="Lampung Cerdas">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500" rel="stylesheet" />
     <link href="<?= base_url('css/bootstrap.min.css') ?>" rel="stylesheet" />
+    <title>Buku Saku</title>
   </head>
   <body>
--- app/Views/search.php
+++ app/Views/search.php
@@ -1,9 +1,12 @@
-<div class="input-group">
-  <div class="form-outline">
-    <input id="search-input" type="search" id="form1" class="form-control" />
-    <label class="form-label" for="form1">Search</label>
+<div class="d-flex align-items-center" style="height: 100vh;">
+  <div class="container">
+    <div class="row justify-content-center mt-4">
+      <div class="col-6">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Cari Pertanyaan" aria-label="Cari" aria-describedby="button-addon2">
+          <button class="btn btn-primary" type="button" id="button-addon2">Button</button>
+        </div>
+      </div>
+    </div>
   </div>
-  <button id="search-button" type="button" class="btn btn-primary">
-    <i class="fas fa-search"></i>
-  </button>
 </div>
