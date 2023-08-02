aa646b022d8f8f8768bb86f6bcfb54360f3423f3
anggrndewi <dewi.120140149@student.itera.ac.id>
2023-08-02 10:15:14 +0700

    Ganti tampilan search

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -4,7 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="author" content="colorlib.com">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+
     <link href="<?= base_url('css/main.css') ?>" rel="stylesheet" />
   </head>
   <body>
